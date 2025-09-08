8ceac25577e2d690ba00d48110c8f721cfa315d4
Mohammed Shahid ul Islam <omgsshahid15@gmail.com>
2025-09-08 12:15:19 +0530

    commit

--- calc/urls.py
+++ calc/urls.py
@@ -5,5 +5,7 @@ urlpatterns=[
     path('', views.home, name='home'),
     path('add',views.add, name='add'),
     path('dashboard',views.dashboard, name='dashboard'),
+    path('products',views.products, name='products'),
+    path('customer/<str:pk_test>/',views.customer, name='customer'),
 
 ]
--- calc/views.py
+++ calc/views.py
@@ -1,4 +1,7 @@
+from .models import *
+
 from django.shortcuts import render
+
 def home(request):
     return render(request, 'home.html',{'name':'Raju'})
 # Create your views here.
@@ -9,4 +12,17 @@ def add(request):
     return render(request,'result.html',{'result':val3})
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    customers=Customer.objects.all()
+    return render(request, 'dashboard.html',{'customers':customers})
+
+def products(request):
+    products=Product.objects.all()
+    return render(request, 'products.html',{'products':products}) 
+
+def customer(request, pk_test):
+    customer=Customer.objects.get(id=pk_test)
+    customers=Customer.objects.all()
+    orders=customer.order_set.all()
+    order_count=orders.count()
+    context={'customers':customers, 'cust':customer,'orders':orders,'ordcount':order_count}
+    return render(request,'customer.html',context)
